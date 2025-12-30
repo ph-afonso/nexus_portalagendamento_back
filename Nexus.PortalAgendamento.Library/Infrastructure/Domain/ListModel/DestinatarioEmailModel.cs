a686ba02eff7c8c35b3238b08eaead0515ee98c2
Phelipe Afonso <phelipe.angelo.ext@tragetta.com.br>
2025-12-30 16:47:25 -0300

    Clean Code

--- Nexus.PortalAgendamento.Library/Infrastructure/Domain/ListModel/DestinatarioEmailModel.cs
+++
@@ -1,3 +0,0 @@
-public class DestinatarioEmailModel { 
-    public string? Email { get; set; }
-    public string? Nome { get; set; } }
--- Nexus.PortalAgendamento.Library/Infrastructure/Domain/ListModel/OcorrenciaImpeditivaModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Nexus.PortalAgendamento.Library.Infrastructure.Domain.ListModel;
-
-public class OcorrenciaImpeditivaModel
-{
-    public int CodOcorrenciaTipo { get; set; } // 140 ou 145
-    public int CodConhecimentos { get; set; }  // Necessário para a procedure
-    public int CodFilial { get; set; }
-}
